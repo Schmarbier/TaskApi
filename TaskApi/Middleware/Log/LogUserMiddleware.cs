2b4cfe3d2bddedf199065e929abb2831dd4d4f98
RomanBorque <roman.borque@sarsy-srl.com>
2025-05-04 21:13:50 -0300

    Se agrega el log del user por middleware para que serilog lo obtenga

---
+++ TaskApi/Middleware/Log/LogUserMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Serilog.Context;
+using System.Security.Claims;
+
+namespace TaskApi.Middleware.Log
+{
+    public class LogUserIdMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LogUserIdMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var userId = context.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            if (!string.IsNullOrWhiteSpace(userId))
+            {
+                using (LogContext.PushProperty("UserId", userId))
+                {
+                    await _next(context); // Todos los logs de este request heredarán esa propiedad
+                }
+            }
+            else
+            {
+                await _next(context);
+            }
+        }
+    }
+}
